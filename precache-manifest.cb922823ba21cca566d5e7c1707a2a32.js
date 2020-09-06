1c0a34754eaba43bbf121cb616ac75729006e7c5
oceantraffic <vadim.ivanyuk.126@gmail.com>
2020-09-06 12:37:29 +0300

    Updates

---
+++ precache-manifest.cb922823ba21cca566d5e7c1707a2a32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f96e4bce2306b331b115921a9825e34",
+    "url": "/weather-channel/index.html"
+  },
+  {
+    "revision": "e96ecef245d083513f90",
+    "url": "/weather-channel/static/css/2.98244dda.chunk.css"
+  },
+  {
+    "revision": "05ab4db031f06cfb9618",
+    "url": "/weather-channel/static/css/main.99e9147e.chunk.css"
+  },
+  {
+    "revision": "e96ecef245d083513f90",
+    "url": "/weather-channel/static/js/2.4e3f6c65.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/weather-channel/static/js/2.4e3f6c65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05ab4db031f06cfb9618",
+    "url": "/weather-channel/static/js/main.4c0b5a97.chunk.js"
+  },
+  {
+    "revision": "b0968b30c098cdbbf709",
+    "url": "/weather-channel/static/js/runtime-main.4dd3253b.js"
+  }
+]);
